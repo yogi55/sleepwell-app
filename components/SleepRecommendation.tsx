aa705703c3fcf6c1445d5179d2b7a5803f5da48e
yogi55 <rabbitshead@gmail.com>
2026-02-27 13:20:33 +0200

    Add SleepRecommendation component for home screen SweetSpot display

---
+++ components/SleepRecommendation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SleepRecommendation = () => {
+    return (
+        <div>
+            <h2>Sweet Spot for Sleep</h2>
+            <p>Try to aim for 7-9 hours of sleep each night to feel your best.</p>
+        </div>
+    );
+};
+
+export default SleepRecommendation;
